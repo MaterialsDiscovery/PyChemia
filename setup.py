c8765447d9b3389ca5bd8f686f486559b3891aae
Guillermo Avendaño-Franco <guilleaf@msn.com>
2015-06-26 16:52:18 -0400

    Moving to setuptools for installation

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pychemia',
-    version='0.1.0',
+    version='0.1.1',
     author='Guillermo Avendano Franco',
     author_email='gtux.gaf@gmail.com',
-    packages=['pychemia',
-              'pychemia.external.ase',
-              'pychemia.external.pymatgen'],
-    url='http://pypi.python.org/pypi/pychemia/',
+    packages=find_packages(),
+    url='https://github.com/MaterialsDiscovery/PyChemia',
     license='LICENSE.txt',
     description='Python framework for Materials Discovery and Design',
     long_description=open('README.md').read(),
@@ -17,4 +15,7 @@ setup(
                       "matplotlib >= 1.2",
                       "ScientificPython >2.6",
                       'spglib', 'pyspglib', 'qmpy'],
+    keywords=["VASP", "ABINIT", "DFTB+", "Octopus", "Fireball", "metaheuristics",
+              "electronic", "structure", "analysis", "materials", "discovery"],
+
 )
