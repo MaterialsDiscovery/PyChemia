2ac33fc6d0927b647fd8d1d86823c43815f29372
Guillermo Avendano-Franco <gufranco@mail.wvu.edu>
2015-06-27 02:23:57 -0400

    Avoiding tests when some modules are not available

--- pychemia/test/test_bin.py
+++ pychemia/test/test_bin.py
@@ -11,57 +11,54 @@ def test_abinit2xyz():
     """
     Test command abinit2xyz             :
     """
-    if not pychemia.HAS_SCIPY or not pychemia.HAS_SCIENTIFIC:
-        return
-    script = 'bin/abi_abinit2xyz.py'
-    args = path + '/abinit_03/abinit.in'
-    subprocess.call(['python', script, args])
-    mol1 = pychemia.io.xyz.load(path + '/abinit_03/abinit_DS1.xyz')
-    mol2 = pychemia.io.xyz.load(path + '/abinit_03/abinit_DS2.xyz')
-    assert (mol1.natom == 16)
-    assert (mol2.natom == 16)
-    os.remove(path + '/abinit_03/abinit_DS1.xyz')
-    os.remove(path + '/abinit_03/abinit_DS2.xyz')
-    os.remove(path + '/abinit_03/abinit.files')
+    if pychemia.HAS_SCIPY and pychemia.HAS_SCIENTIFIC:
+        print 'Running abinit2xyz', pychemia.HAS_SCIPY, pychemia.HAS_SCIENTIFIC
+        script = 'bin/abi_abinit2xyz.py'
+        args = path + '/abinit_03/abinit.in'
+        subprocess.call(['python', script, args])
+        mol1 = pychemia.io.xyz.load(path + '/abinit_03/abinit_DS1.xyz')
+        mol2 = pychemia.io.xyz.load(path + '/abinit_03/abinit_DS2.xyz')
+        assert (mol1.natom == 16)
+        assert (mol2.natom == 16)
+        os.remove(path + '/abinit_03/abinit_DS1.xyz')
+        os.remove(path + '/abinit_03/abinit_DS2.xyz')
+        os.remove(path + '/abinit_03/abinit.files')
 
 
 def test_xyz2abinit():
     """
     Test command xyz2abinit             :
     """
-    if not pychemia.HAS_SCIPY or not pychemia.HAS_SCIENTIFIC:
-        return
-    script = 'bin/abi_xyz2abinit.py'
-    args = path + '/xyz/chlorophyll.xyz'
-    subprocess.call(['python', script, args])
-    inp = pychemia.code.abinit.InputVariables(path + '/xyz/chlorophyll.xyz.in')
-    assert (inp.get_value('natom') == 140)
-    os.remove(path + '/xyz/chlorophyll.xyz.in')
-    return inp
+    if pychemia.HAS_SCIPY and pychemia.HAS_SCIENTIFIC:
+        script = 'bin/abi_xyz2abinit.py'
+        args = path + '/xyz/chlorophyll.xyz'
+        subprocess.call(['python', script, args])
+        inp = pychemia.code.abinit.InputVariables(path + '/xyz/chlorophyll.xyz.in')
+        assert (inp.get_value('natom') == 140)
+        os.remove(path + '/xyz/chlorophyll.xyz.in')
+        return inp
 
 
 def test_plot_bonds():
     """
     Test command plot_bonds             :
     """
-    if not pychemia.HAS_SCIPY or not pychemia.HAS_SCIENTIFIC:
-        return
-    script = 'bin/abi_plot_bonds.py'
-    arg1 = path + '/abinit_01/abinit.files:11'
-    arg2 = path + '/abinit_01/abinit.files:21'
-    subprocess.call(['python', script, arg1, arg2])
-    os.remove("bonds.txt")
-    os.remove("bonds.pdf")
+    if pychemia.HAS_SCIPY and pychemia.HAS_SCIENTIFIC:
+        script = 'bin/abi_plot_bonds.py'
+        arg1 = path + '/abinit_01/abinit.files:11'
+        arg2 = path + '/abinit_01/abinit.files:21'
+        subprocess.call(['python', script, arg1, arg2])
+        os.remove("bonds.txt")
+        os.remove("bonds.pdf")
 
 
 def test_plot_hist():
     """
     Test command plot_hist              :
     """
-    if not pychemia.HAS_SCIPY or not pychemia.HAS_SCIENTIFIC:
-        return
-    script = 'bin/abi_plot_hist.py'
-    arg1 = path + '/abinit_01/abinit.files'
-    arg2 = '11'
-    subprocess.call(['python', script, arg1, arg2])
-    os.remove(path + '/abinit_01/abinit-o_DS11.pdf')
+    if pychemia.HAS_SCIPY and pychemia.HAS_SCIENTIFIC:
+        script = 'bin/abi_plot_hist.py'
+        arg1 = path + '/abinit_01/abinit.files'
+        arg2 = '11'
+        subprocess.call(['python', script, arg1, arg2])
+        os.remove(path + '/abinit_01/abinit-o_DS11.pdf')
