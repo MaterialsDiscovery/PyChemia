cf30ee20bd2ebf28f1040592aab298b418f69169
Guillermo Avendano-Franco <gufranco@mail.wvu.edu>
2015-06-28 08:20:17 -0400

    Avoiding tests when some modules are not available

--- pychemia/code/abinit/__init__.py
+++ pychemia/code/abinit/__init__.py
@@ -6,13 +6,19 @@ ABINIT '.files' files
 ABINIT '_OUT.nc' output files
 
 """
+try:
+    import scipy
 
-from _abifiles import AbiFiles
-from _input import InputVariables, netcdf2dict
-from _utils import get_all_psps, psp_name, xyz2input, plot_simple, abihelp
-from _parser import parser
-from _htmlparser import MyHTMLParser
-from _tasks import RelaxPopulation
-from _abinit import AbinitJob
+
+    from _abifiles import AbiFiles
+    from _input import InputVariables, netcdf2dict
+    from _utils import get_all_psps, psp_name, xyz2input, plot_simple, abihelp
+    from _parser import parser
+    from _htmlparser import MyHTMLParser
+    from _tasks import RelaxPopulation
+    from _abinit import AbinitJob
+
+except InportError:
+    print 'SCIPY not present'
 
 # __all__ = filter(lambda s: not s.startswith('_'), dir())
