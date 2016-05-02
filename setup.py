859a6ec5e54baf949c984460bc5d09e84acf4e48
guilleaf <gufranco@mail.wvu.edu>
2016-05-02 12:39:09 -0400

    Making travis work (Trial 13)

--- setup.py
+++ setup.py
@@ -11,9 +11,6 @@ setup(
     description='Python framework for Materials Discovery and Design',
     long_description=open('README.md').read(),
     install_requires=['numpy >= 1.5',
-                      'scipy >= 0.17.0',
-                      'matplotlib >= 1.5.0',
-                      'spglib >= 1.9.0',
-                      'pyhull >= 1.5.4'],
+                      'scipy >= 0.17.0'],
     keywords=["electronic", "structure", "analysis", "materials", "discovery", "metaheuristics"],
 )
