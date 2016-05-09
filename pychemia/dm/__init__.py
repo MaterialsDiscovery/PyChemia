695df73aaf5b4f369be054dbddc0502f09047c4a
guilleaf <gufranco@mail.wvu.edu>
2016-05-09 19:54:34 -0400

    Presorting composition to pass tests

--- pychemia/dm/__init__.py
+++ pychemia/dm/__init__.py
@@ -1,9 +1,9 @@
 """
 Routines related to Data mining
 """
-from pychemia import HAS_NETWORKX
+from pychemia import HAS_NETWORKX, HAS_PYMONGO
 
-if HAS_NETWORKX:
+if HAS_NETWORKX and HAS_PYMONGO:
     from network import NetworkAnalysis
 
 # __all__ = filter(lambda s: not s.startswith('_'), dir())
