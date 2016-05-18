33b59bd9355b64c75dc058afd4d5bd5786e5d998
guilleaf <gufranco@mail.wvu.edu>
2016-05-18 15:02:18 -0400

    Reverting positions2reduced

--- pychemia/external/ase/__init__.py
+++ pychemia/external/ase/__init__.py
@@ -5,6 +5,6 @@ Routines related to Data mining
 from pychemia import HAS_ASE, HAS_SPGLIB
 
 if HAS_ASE and HAS_SPGLIB:
-    from _ase import ase2pychemia, pychemia2ase
+    from ._ase import ase2pychemia, pychemia2ase
 
 # __all__ = filter(lambda s: not s.startswith('_'), dir())
