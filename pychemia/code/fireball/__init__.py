9bbcc6b9f80e0736e7d679a9c022b7a4d465d952
guilleaf <gufranco@mail.wvu.edu>
2016-08-10 15:03:15 -0400

    Adding more routines for fireball

--- pychemia/code/fireball/__init__.py
+++ pychemia/code/fireball/__init__.py
@@ -1,1 +1,2 @@
-from .fireball import FireBall, read_fireball_stdout, read_geometry_bas, write_geometry_bas, get_fdata_info
+from .fireball import FireBall, read_fireball_stdout, read_geometry_bas, write_geometry_bas, get_fdata_info, read_eigen
+from .fireball import read_final_fireball_relax, read_fireball_in
