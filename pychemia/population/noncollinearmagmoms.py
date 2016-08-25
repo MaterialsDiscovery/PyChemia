5b8a05900d363be163a8e3e799126c9736b18304
guilleaf <gufranco@mail.wvu.edu>
2016-08-25 16:27:16 -0400

    Improving MAGMOM Global search

--- pychemia/population/noncollinearmagmoms.py
+++ pychemia/population/noncollinearmagmoms.py
@@ -90,7 +90,7 @@ class NonCollinearMagMoms(Population):
 
     def is_evaluated(self, entry_id):
         entry = self.get_entry(entry_id, {'_id': 0, 'properties': 1})
-        if entry['properties']['energy'] is not None:
+        if 'energy' in entry['properties'] and entry['properties']['energy'] is not None:
             return True
         else:
             return False
