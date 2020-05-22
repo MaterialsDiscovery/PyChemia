db511ce5976f84be991b580a468dfeb721b7ac32
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-05-22 18:44:50 -0400

    Added import pyximport to lennardjones files

--- pychemia/code/lennardjones/__init__.py
+++ pychemia/code/lennardjones/__init__.py
@@ -2,6 +2,8 @@
 Includes class LennardJones and several methods to relax and compute forces and energies from Lennard-Jones clusters
 
 """
+import pyximport
+pyximport.install()
 
 from .lj import LennardJones, lj_compact_evaluate
 from .lj_utils import lj_energy, lj_forces, lj_gradient
--- pychemia/code/lennardjones/lj.py
+++ pychemia/code/lennardjones/lj.py
@@ -1,10 +1,17 @@
+import pyximport
+pyximport.install()
+
 import numpy as np
-from pychemia import pcm_log
 import scipy.optimize
 import scipy.spatial
+from pychemia import pcm_log
 from pychemia.utils.mathematics import length_vectors
 from pychemia.utils.periodic import mass
-from .lj_utils import lj_forces, lj_energy, lj_gradient
+
+from .lj_utils import lj_energy, lj_forces, lj_gradient
+
+
+
 
 
 class LennardJones:
@@ -16,7 +23,7 @@ class LennardJones:
             ljparams = {}
             for i in self.structure.species:
                 for j in self.structure.species:
-                    ljparams[i + '-' + j] = {'sigma': 1.0, 'epsilon': 1.0}
+                    ljparams[i + "-" + j] = {"sigma": 1.0, "epsilon": 1.0}
         self.ljparams = ljparams
         self.cp = cp
         self.sigmas = np.zeros((structure.natom, structure.natom))
@@ -26,12 +33,14 @@ class LennardJones:
                 self.sigmas[i, j], self.epsilons[i, j] = self.get_sigma_epsilon(i, j)
 
     def get_forces(self):
-        return lj_forces(self.structure.positions, self.sigmas, self.epsilons, self.cp).reshape((-1, 3))
+        return lj_forces(
+            self.structure.positions, self.sigmas, self.epsilons, self.cp
+        ).reshape((-1, 3))
 
     def get_magnitude_forces(self):
         return length_vectors(self.get_forces())
 
-    def steepest_descent(self, dt=1, tolerance=1E-3):
+    def steepest_descent(self, dt=1, tolerance=1e-3):
         forces = self.get_forces()
         while np.max(self.get_magnitude_forces()) > tolerance:
             for i in range(self.structure.natom):
@@ -41,8 +50,10 @@ class LennardJones:
             forces = self.get_forces()
             maxforce = np.max(self.get_magnitude_forces())
             dt = max(100.0 / maxforce, 1.0)
-            pcm_log.debug('Steepest Descent maxforce= %9.3E   density=%7.4f dt=%7.3f' % (maxforce,
-                                                                                         self.structure.density, dt))
+            pcm_log.debug(
+                "Steepest Descent maxforce= %9.3E   density=%7.4f dt=%7.3f"
+                % (maxforce, self.structure.density, dt)
+            )
 
     def get_sigma_epsilon(self, i, j):
         specie1 = self.structure.symbols[i]
@@ -50,22 +61,24 @@ class LennardJones:
         sort_species = [specie1, specie2]
         sort_species.sort()
 
-        sigma = self.ljparams[sort_species[0] + '-' + sort_species[1]]['sigma']
-        epsilon = self.ljparams[sort_species[0] + '-' + sort_species[1]]['epsilon']
+        sigma = self.ljparams[sort_species[0] + "-" + sort_species[1]]["sigma"]
+        epsilon = self.ljparams[sort_species[0] + "-" + sort_species[1]]["epsilon"]
 
         return float(sigma), float(epsilon)
 
     def get_energy(self):
         return lj_energy(self.structure.positions, self.sigmas, self.epsilons, self.cp)
 
-    def local_minimization(self, method='BFGS', gtol=1E-4, soft_max_ncalls=5, hard_max_ncalls=20):
+    def local_minimization(
+        self, method="BFGS", gtol=1e-4, soft_max_ncalls=5, hard_max_ncalls=20
+    ):
 
-        if method in ['Nelder-Mead', 'Powell']:
+        if method in ["Nelder-Mead", "Powell"]:
             jac = None
-            options = {'maxiter': 100, 'disp': False}
+            options = {"maxiter": 100, "disp": False}
         else:
             jac = lj_gradient
-            options = {'gtol': 0.1 * gtol, 'disp': False}
+            options = {"gtol": 0.1 * gtol, "disp": False}
 
         x0 = self.structure.positions.flatten()
 
@@ -75,15 +88,25 @@ class LennardJones:
 
         while True:
 
-            res = scipy.optimize.minimize(lj_energy, x0, args=(self.sigmas, self.epsilons, self.cp), method=method,
-                                          jac=jac, options=options)
+            res = scipy.optimize.minimize(
+                lj_energy,
+                x0,
+                args=(self.sigmas, self.epsilons, self.cp),
+                method=method,
+                jac=jac,
+                options=options,
+            )
 
-            if method in ['BFGS', 'CG']:
-                maxforce = np.max(np.apply_along_axis(np.linalg.norm, 1, np.array(res.jac).reshape((-1, 3))))
-                pcm_log.debug('[%2d/%2d] MaxForce/gtol= %9.3E/%9.3E  Value= %12.5f' % (soft_call,
-                                                                                       soft_max_ncalls,
-                                                                                       maxforce, gtol,
-                                                                                       res.fun))
+            if method in ["BFGS", "CG"]:
+                maxforce = np.max(
+                    np.apply_along_axis(
+                        np.linalg.norm, 1, np.array(res.jac).reshape((-1, 3))
+                    )
+                )
+                pcm_log.debug(
+                    "[%2d/%2d] MaxForce/gtol= %9.3E/%9.3E  Value= %12.5f"
+                    % (soft_call, soft_max_ncalls, maxforce, gtol, res.fun)
+                )
 
                 x0 = res.x
                 soft_call += 1
@@ -91,15 +114,25 @@ class LennardJones:
                 if maxforce < gtol:
                     break
                 if soft_call > soft_max_ncalls:
-                    pcm_log.debug('Anomalous condition, maxforce= %9.3E gtol= %9.3E' % (maxforce, gtol))
+                    pcm_log.debug(
+                        "Anomalous condition, maxforce= %9.3E gtol= %9.3E"
+                        % (maxforce, gtol)
+                    )
                     pos = np.array(x0).reshape((-1, 3))
-                    mindis = np.min(np.array(np.array(scipy.spatial.distance_matrix(pos, pos)) +
-                                             100 * np.eye(len(pos))).flatten())
+                    mindis = np.min(
+                        np.array(
+                            np.array(scipy.spatial.distance_matrix(pos, pos))
+                            + 100 * np.eye(len(pos))
+                        ).flatten()
+                    )
                     x0 = np.array(1.0 / mindis * pos).flatten()
                     soft_call = 1
                 if hard_call > hard_max_ncalls:
-                    pcm_log.debug('Could not reach target forces, maxforce= %9.3E gtol= %9.3E' % (maxforce, gtol))
-                    pcm_log.debug('Hard limit reached, aborting local minimization')
+                    pcm_log.debug(
+                        "Could not reach target forces, maxforce= %9.3E gtol= %9.3E"
+                        % (maxforce, gtol)
+                    )
+                    pcm_log.debug("Hard limit reached, aborting local minimization")
                     break
             else:
                 break
@@ -120,11 +153,13 @@ def lj_compact_evaluate(structure, gtol, minimal_density):
         relax = lj.local_minimization(gtol=gtol)
         structure.set_positions(relax.x.reshape((-1, 3)))
         finden = structure.density
-        pcm_log.debug('Compacting Cluster (target_density=%7.3f): Density I= %7.3f   F= %7.3f' % (minimal_density,
-                                                                                                  iniden, finden))
+        pcm_log.debug(
+            "Compacting Cluster (target_density=%7.3f): Density I= %7.3f   F= %7.3f"
+            % (minimal_density, iniden, finden)
+        )
         k += 1
         if k > 10:
-            pcm_log.debug('I tried too much...')
+            pcm_log.debug("I tried too much...")
             break
 
     lj = LennardJones(structure)
